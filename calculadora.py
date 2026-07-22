979b90f2561465cc11fc77493aad0ed1c26b0963
sebastianvilca2011-beep <sebastianvilca2011@gmail.com>
2026-07-22 15:40:54 -0500

    PUDATE CALCULADORA
    AGREGAR ESTA , SEBASTIAN VILCA

--- calculadora.py
+++ calculadora.py
@@ -1,9 +1,20 @@
-## CALCULADORA - VERSIÓN 1
-# Autor(a): NOMBRE APELLIDO
+## CALCULADORA - VERSIÓN 2
+# Autor(a): sebastian vilca
 # ===========================================
+print("calculadora")
+
 num1 = float(input("Ingrese el primer número: "))
 num2 = float(input("Ingrese el segundo número: "))
 
-suma = num1 + num2
+print("\n 1. sumar")
+print("\ 2. restar")
 
-print("Resultado:", suma)
+opcion = input("\n seleccione una opcion: ")
+
+if opcion == "1":
+  print("\n la suma es:", num1 + num2
+
+        elif opcione == "2":
+        print("\n la resta es:" , num1 - num2)
+        else:
+        print("\n opcion no validad.")
