230b94472f9a4d15a716ab559531ad598e569b3d
sebastianvilca2011-beep <sebastianvilca2011@gmail.com>
2026-07-22 15:20:38 -0500

    Add files via upload
    codigo de operaciones vilca

---
+++ calculadora.py
@@ -0,0 +1,9 @@
+## CALCULADORA - VERSIÓN 1
+# Autor(a): NOMBRE APELLIDO
+# ===========================================
+num1 = float(input("Ingrese el primer número: "))
+num2 = float(input("Ingrese el segundo número: "))
+
+suma = num1 + num2
+
+print("Resultado:", suma)
